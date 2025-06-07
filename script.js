cd98362a03c18d60099f791b752305de0c192801
Gianni Randone <giannirandone@users.noreply.github.com>
2025-06-08 00:27:13 +0200

    Lektion 38

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ btn.addEventListener("click", function () {
   }
 });
 
+/*
+
 let voteInteresting = 23;
 let votesMindblowing = 5;
 const text = "lisbon is the capital of Portugal";
@@ -29,16 +31,46 @@ const isCorrect = votesFalse < totalUpvotes;
 console.log(isCorrect);
 
 console.log(parseInt("24.533ccc"));
+*/
 
 function calcFactAge(year) {
   const currentYear = new Date().getFullYear();
   const age = currentYear - year;
 
-  return age;
-
   //return currentYear - year;
+  return age < 0 ? "Yor're not in the future bre" : age;
 }
 
 const age1 = calcFactAge(2015);
 console.log(age1);
 console.log(calcFactAge(2020));
+console.log(calcFactAge(2037));
+
+let voteInteresting = 20;
+let votesMindblowing = 5;
+
+if (voteInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (voteInteresting > votesMindblowing) {
+  console.log("Interesting fact!");
+} else if (voteInteresting < votesMindblowing) {
+  console.log("Mindblowing fact!!");
+}
+
+// falsy values: o, "", null, undefined
+
+if (votesMindblowing) {
+  console.log("MINDBLOWING FACT!!!");
+} else {
+  console.log("Not so special...");
+}
+
+let votesFalse = 77;
+const totalUpvotes = voteInteresting + votesMindblowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true"
+    : "Might be false, check more sources...";
+
+//alert(message);
