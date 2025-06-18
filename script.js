632af877c70a1901c1d6215bc4f0531f6d8fe8ee
Gianni Randone <giannirandone@users.noreply.github.com>
2025-06-18 13:56:48 +0200

    Lektion 43

--- script.js
+++ script.js
@@ -99,17 +99,80 @@ console.log(str);
 */
 
 const fact = ["Lisbon is the capital of Portugal", 2015, true, "something"];
-console.log(fact);
 console.log(fact[0]);
 console.log(fact.length);
 console.log(fact[fact.length - 1]);
 
 const [text, createdIn] = fact;
-console.log(text, createdIn);
-
 const newFact = [...fact, "society"];
 console.log(newFact);
 
+/*[2, 4, 6, 8].forEach(function (el) {
+  console.log(el);
+});*/
+
+/*const times10 = [2, 4, 6, 8].map(function (el) {
+  return el * 10;
+});*/
+const times10 = [2, 4, 6, 8].map((el) => el * 10);
+console.log(times10);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+console.log(CATEGORIES.map((el) => el.name));
+console.log(CATEGORIES.map((el) => el.color));
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const calcFactAge2 = (year) =>
+  year <= new Date().getFullYear()
+    ? new Date().getFullYear() - year
+    : `You're not in the future bre... Year need to be less or equal ${new Date().getFullYear()}.`;
+
+console.log(initialFacts.map((el) => calcFactAge2(el.createdIn)));
+
+/*
 const factObj = {
   text: "Lisbon is the capital of Portugal",
   category: "society",
@@ -128,3 +191,4 @@ console.log(factObj["text"]);
 const { category, isCorrect } = factObj;
 console.log(category);
 console.log(factObj.createSummary());
+*/
