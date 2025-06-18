4dca8e0cdc81debccb1456ab0f7ba64072024771
Gianni Randone <giannirandone@users.noreply.github.com>
2025-06-18 12:45:09 +0200

    Lektion 42

--- script.js
+++ script.js
@@ -104,8 +104,27 @@ console.log(fact[0]);
 console.log(fact.length);
 console.log(fact[fact.length - 1]);
 
-const [text, createdIn, isCorrect] = fact;
-console.log(text, isCorrect);
+const [text, createdIn] = fact;
+console.log(text, createdIn);
 
 const newFact = [...fact, "society"];
 console.log(newFact);
+
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact "${
+      this.text
+    }" is from the category "${this.category.toUpperCase()}"`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj["text"]);
+
+const { category, isCorrect } = factObj;
+console.log(category);
+console.log(factObj.createSummary());
